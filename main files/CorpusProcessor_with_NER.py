4a9343d9004474210844e36b78ea9d647bfa6b79
Krishna Tripathi <121068842+kkrishna24@users.noreply.github.com>
2024-01-25 11:19:35 -0600

    Fixed error in CorpusProcessor_with_NER.py

--- main files/CorpusProcessor_with_NER.py
+++ main files/CorpusProcessor_with_NER.py
@@ -23,7 +23,7 @@ import itertools
 # nltk.download()  # Run this line whenever project is to be executed for the first time. Triggers nltk data-packs to download
                    # to local system.
 
-FILEPATH = '../Input Files/internet_archive_scifi_v3.txt''
+FILEPATH = '../Input Files/internet_archive_scifi_v3.txt'
 
 # Path to a sample corpus
 FILEPATH = '../Input Files/small_sample_text_for_testing.txt' # DELETE (or comment-out) this line in order to process the "real" corpus
